c3a34ba79d64d75bff8ea034b960298ea8f2bd2a
salmansrabon <salmansrabon@gmail.com>
2024-01-26 22:22:25 +0600

    upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/1844288-dd71af23-ac2d-46b9-a25d-78e38466ae00?access_key=${process.env.ACCESS_KEY}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
